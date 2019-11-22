94619139f068421a3bf202f65d516e6b8108f805
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2019-11-22 09:20:03 +0100

    Updates

---
+++ precache-manifest.6438b2512d12fc93a7b9d4bd85060cdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b3963762a939878a1803bdaf611301d",
+    "url": "/netflix-react/index.html"
+  },
+  {
+    "revision": "21267e628d2ca246ab4f",
+    "url": "/netflix-react/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "d71e24d6c688ca75e63b",
+    "url": "/netflix-react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "21267e628d2ca246ab4f",
+    "url": "/netflix-react/static/js/2.df45507c.chunk.js"
+  },
+  {
+    "revision": "d71e24d6c688ca75e63b",
+    "url": "/netflix-react/static/js/main.aeb4ef41.chunk.js"
+  },
+  {
+    "revision": "5ed306f694ba4f12f903",
+    "url": "/netflix-react/static/js/runtime-main.3d7f3b15.js"
+  }
+]);

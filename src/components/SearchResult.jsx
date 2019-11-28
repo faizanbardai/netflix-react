343d8483195efbff456c07b52072c714819c7b7f
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2019-11-28 10:51:13 +0100

    "No Movie Found" Error handling

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -8,34 +8,37 @@ class SearchResult extends Component {
         loading: true
     }
     render() {
-        let { searchQuery } = this.state;
-        let { searchArray } = this.state;
-        if (this.state.loading) {
+        let { searchQuery, searchArray, loading, error } = this.state;
+        if (error) {
+            return (
+                <div>{error}</div>
+            )
+        } else if (loading) {
             return (
                 <div className="spinner-grow" role="status">
                     <span className="sr-only">Loading...</span>
                 </div>
             )
-        } else {
+        } else if (searchArray.Search) {
             return (
                 <>
                     <h3 className="display-4">Search: {searchQuery}</h3>
-                    <div className="row">
+                    {searchArray.Search.length > 0 && <div className="row">
                         {searchArray.Search.map((movie, index) => {
                             return (
                                 <div key={index.toString()} className="col-6 col-sm-4 col-md-3 col-lg-2 px-1">
                                     <Link to={"/movie-detail/" + movie.imdbID}>
-                                        <img 
-                                            src={movie.Poster === "N/A"? "https://via.placeholder.com/300x445": movie.Poster} 
-                                            style={{ width: "100%" }} 
-                                            className="mb-2" 
-                                            alt={movie.Title} 
+                                        <img
+                                            src={movie.Poster === "N/A" ? "https://via.placeholder.com/300x445" : movie.Poster}
+                                            style={{ width: "100%" }}
+                                            className="mb-2"
+                                            alt={movie.Title}
                                         />
                                     </Link>
                                 </div>
                             )
                         })}
-                    </div>
+                    </div>}
                 </>
             )
         }
@@ -43,19 +46,28 @@ class SearchResult extends Component {
     componentDidMount = async () => {
         let searchQuery = this.props.match.params.searchQuery;
         let searchArray = await OMDBGETSearch(searchQuery)
-        this.setState({
-            searchArray: searchArray,
-            loading: false
-        })
+
+        searchArray.Error ? this.setState({ error: searchArray.Error }) :
+
+            this.setState({
+                searchArray: searchArray,
+                loading: false,
+                error: false
+            })
     }
     componentDidUpdate = async (prevProps) => {
         if (this.props.location.pathname !== prevProps.location.pathname) {
             let searchQuery = this.props.match.params.searchQuery;
             let searchArray = await OMDBGETSearch(searchQuery)
-            this.setState({
-                searchQuery: searchQuery,
-                searchArray: searchArray
-            })
+
+            searchArray.Error ? this.setState({ error: searchArray.Error }) :
+
+                this.setState({
+                    searchQuery: searchQuery,
+                    searchArray: searchArray,
+                    error: false,
+                    loading: false
+                })
         }
     }
 }

f5aa8ba7de0ebe8926cbd695944b0b294cf878b3
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2019-12-01 17:49:15 +0100

    Added loader to WriteComment

--- src/components/WriteComment.jsx
+++ src/components/WriteComment.jsx
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import POSTComment from '../API/POSTComment';
 
 class WriteComment extends Component {
-    
+    state = {
+        submittingComment: false
+    }
+
     captureComment = async (e) => {
         e.preventDefault();
         let commentObject = {
@@ -10,42 +13,51 @@ class WriteComment extends Component {
             "rate": document.querySelector("#rating").value,
             "elementId": this.props.movieID
         };
+        this.setState({submittingComment: true});
         let message = await POSTComment(commentObject);
-        document.querySelector("#comment").value="";
-        document.querySelector("#rating").value=5;
         this.props.addComments(message);
+        this.setState({submittingComment: false});
+        document.querySelector("#comment").value = "";
+        document.querySelector("#rating").value = 5;
     }
     render() {
+
         return (
-            <form onSubmit={this.captureComment}>
-                <div className="form-group">
-                    {/* <label for="comment">Example textarea</label> */}
-                    <textarea
-                        required
-                        className="form-control mb-2"
-                        id="comment"
-                        rows="3"
-                        placeholder="Write a new comment..."
-                    >
-                    </textarea>
-                    <div className="input-group">
-                        <select
-                            className="custom-select"
-                            id="rating"
-                            aria-label="Example select with button addon">
-                            <option value="5">Five</option>
-                            <option value="4">Four</option>
-                            <option selected value="3">Three</option>
-                            <option value="2">Two</option>
-                            <option value="1">One</option>
-                        </select>
-                        <div className="input-group-append">
-                            <button className="btn btn-outline-secondary" type="submit">Submit</button>
-                        </div>
-                    </div>
-                </div>
-            </form>
-        );
+            <>
+                {this.state.submittingComment ? <>Loading... </> :
+                    (
+                        <form onSubmit={this.captureComment}>
+                            <div className="form-group">
+                                {/* <label for="comment">Example textarea</label> */}
+                                <textarea
+                                    required
+                                    className="form-control mb-2"
+                                    id="comment"
+                                    rows="3"
+                                    placeholder="Write a new comment..."
+                                >
+                                </textarea>
+                                <div className="input-group">
+                                    <select
+                                        className="custom-select"
+                                        id="rating"
+                                        aria-label="Example select with button addon">
+                                        <option value="5">Five</option>
+                                        <option value="4">Four</option>
+                                        <option selected value="3">Three</option>
+                                        <option value="2">Two</option>
+                                        <option value="1">One</option>
+                                    </select>
+                                    <div className="input-group-append">
+                                        <button className="btn btn-outline-secondary" type="submit">Submit</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    )
+                }
+            </>
+        )
     }
 }
 
